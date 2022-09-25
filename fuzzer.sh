7d48865da2288bf31172a1d02ba536f2144306ce
Greatestdeveloper <91232198+Greatestdeveloper@users.noreply.github.com>
2022-09-25 20:50:01 +0530

    Update fuzzer.sh

--- fuzzer.sh
+++ fuzzer.sh
@@ -2,10 +2,15 @@
 
 cd ~
 sudo apt-get install git
-#opening new terminal for running fuzzer
 
-gnome-terminal
-git clone https://github.com/Greatestdeveloper/fuzz_testing
+#opening new terminal for running fuzzer
+#commands are not getting executed in the new terminal opened.
+#gnome-terminal
+
+echo "---------------Fork the given github repo as paste the forked url here--------------"
+echo "https://github.com/Greatestdeveloper/hack-a-drone"
+read url 
+git clone --recurse-submodules $url
 git submodule update --init --recursive
 cd fuzz_testing 
 (cd mavlink/pymavlink/ && tools/mavgen.py --lang=C --wire-protocol=2.0 --output=../../generated/include/mavlink/v2.0 ../message_definitions/v1.0/common.xml)
