de3241b1b20687e501569f93c9236c7b32a18f6e
Greatestdeveloper <91232198+Greatestdeveloper@users.noreply.github.com>
2022-09-26 20:36:22 +0530

    Update fuzzer.sh

--- fuzzer.sh
+++ fuzzer.sh
@@ -2,8 +2,8 @@
 
 cd ~
 sudo apt-get install git
-sudo apt-get install cmake 
-sudo apt-get install clang
+sudo apt-get install cmake && sudo apt-get upgrade cmake
+sudo apt-get install clang && sudo apt-get upgrade clang
 
 #opening new terminal for running fuzzer
 #commands are not getting executed in the new terminal opened.
@@ -14,9 +14,8 @@ echo "https://github.com/Greatestdeveloper/hack-a-drone"
 read url 
 git clone --recurse-submodules $url
 git submodule update --init --recursive
-sudo apt-get update && sudo apt-get upgrade 
-
 cd fuzz_testing 
+sudo apt-get update && sudo apt-get upgrade 
 (cd mavlink/pymavlink/ && tools/mavgen.py --lang=C --wire-protocol=2.0 --output=../../generated/include/mavlink/v2.0 ../message_definitions/v1.0/common.xml)
 cmake -Bbuild src && cmake --build build && build/parser_with_assembled_message CORPUS_DIR/
 
