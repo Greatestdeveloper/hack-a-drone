3d724d60255db114cc089a06ec9783a7117d490e
Greatestdeveloper <91232198+Greatestdeveloper@users.noreply.github.com>
2022-09-26 20:27:25 +0530

    Update fuzzer.sh

--- fuzzer.sh
+++ fuzzer.sh
@@ -14,6 +14,8 @@ echo "https://github.com/Greatestdeveloper/hack-a-drone"
 read url 
 git clone --recurse-submodules $url
 git submodule update --init --recursive
+sudo apt-get update && sudo apt-get upgrade 
+
 cd fuzz_testing 
 (cd mavlink/pymavlink/ && tools/mavgen.py --lang=C --wire-protocol=2.0 --output=../../generated/include/mavlink/v2.0 ../message_definitions/v1.0/common.xml)
 cmake -Bbuild src && cmake --build build && build/parser_with_assembled_message CORPUS_DIR/
